7fa0fc5ca32ae2cf23afd929d4cfe9bf6424b6ba
heverton.nascimento2020@gmail.com <heverton.noliveira@senacsp.edu.br>
2023-02-07 08:10:26 -0300

    ExercicioJs

---
+++ dom2.js
@@ -0,0 +1,14 @@
+function clique() {
+    document.querySelector('p').innerText = "Você Clicou"
+
+}
+
+function passoumouse(){
+
+
+    document.querySelector('p').innerText = "Você passou o mouse"
+}
+
+function tiroumouse(){
+
+    document.querySelector('p').innerText = "Você tirou o mouse" }
---
+++ script.js
@@ -0,0 +1,24 @@
+function clickMe(){
+
+    console.log("Fui clicado!")
+}
+
+function FocusMe(){
+
+    console.log ("Fui focado!")
+
+}
+
+function changeMe(){
+    console.log ("Fui Alterado!")
+}
+
+function overme () {
+    console.log ("Mouse aqui!")
+}
+
+
+function blurMe () {
+    console.log ("Fui Desfocado!")
+}
+
