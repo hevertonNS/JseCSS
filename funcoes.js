99a91130f063e82e2e9f9f0eb6b5ef763a154b07
heverton.nascimento2020@gmail.com <heverton.noliveira@senacsp.edu.br>
2023-02-08 11:11:18 -0300

    atualizacao js

---
+++ funcoes.js
@@ -0,0 +1,31 @@
+function fundoblue(){
+
+  let element = document.querySelector('#style-text')
+  element.style.backgroundColor = "blue" 
+
+
+}
+
+function fundotrans(){
+
+    let element = document.querySelector('#style-text')
+    element.style.backgroundColor = "transparent"  
+
+    
+
+}
+
+function fontered(){
+
+    let element = document.querySelector("#style-text").style.color = "red";
+
+
+
+}
+
+function fontenormal(){
+
+    let element = document.querySelector("#style-text").style.color = "black";
+
+
+}
